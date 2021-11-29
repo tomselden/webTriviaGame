fa8a62cb593f9e78fa713fa8d29e03a0d6e923c3
Thomas Selden <thomasselden42@gmail.com>
2021-11-29 16:35:20 -0500

    changed structure of html start page and added css styles to it, also added questions to use to test quiz functionality

--- app.js
+++ app.js
@@ -1,3 +1,58 @@
-// quiz js code 
-console.log("im working!")
+// quiz js code
+
+// questions
+const nbaQuest = [
+  {
+    question: "Who plays in MSG (Madison Square Garden)?",
+    answOne: "Dallas Mavericks",
+    answTwo: "New York Knicks",
+    answThree: "Miami Heat",
+    answFour: "Boston Celtics",
+    correctAnsw: "answTwo",
+  },
+
+  {
+    question: "What team does LeBron James currently play for?",
+    answOne: "Los Angeles Lakers",
+    answTwo: "Cleveland Cavaliers",
+    answThree: "Miami Heat",
+    answFour: "Memphis Grizzlies",
+    correctAnsw: "answOne",
+  },
+
+  {
+    question: "This season marks the NBA's _____ anniversary.",
+    answOne: "80th",
+    answTwo: "100th",
+    answThree: "50th",
+    answFour: "75th",
+    correctAnsw: "answFour",
+  },
+
+  {
+    question: "What team did Carmelo Anthony NOT play for?",
+    answOne: "Denver Nuggets",
+    answTwo: "New York Knicks",
+    answThree: "Miami Heat",
+    answFour: "Oklahoma City Thunder",
+    correctAnsw: "answThree",
+  },
+
+  {
+    question: "Who plays in MSG (Madison Square Garden)?",
+    answOne: "Dallas Mavericks",
+    answTwo: "New York Knicks",
+    answThree: "Miami Heat",
+    answFour: "Boston Celtics",
+    correctAnsw: "answTwo",
+  },
+];
+
+// getting category of questions
+
+let retrieveNBAQues = document.getElementById("PlayNBAQuiz")
+    retrieveNBAQues.addEventListener("onclick", function(nbaQuest){
+        let questionOne = document.getElementById("question")
+
+})
 
---
+++ docs/menubrainstorming.md
@@ -0,0 +1,7 @@
+# what we want to happen 
+-start screen with three different categories of quiz type 
+-user chooses one category and then the application gets the questions
+
+# how to build that 
+-two screens, one the category selection and the other one the actual quiz game 
+-when i select a category then 
